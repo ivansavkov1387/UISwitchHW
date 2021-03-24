974ca41ad4f1a6e835fb897c6a37a1964388f2f5
Ivan Savkov <ivansavkov1387@gmail.com>
2021-03-24 20:33:39 +0300

    Initial Commit

---
+++ UISwitchHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UISwitchHW
+//
+//  Created by Иван on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
